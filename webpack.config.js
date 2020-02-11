5ea00e6138c6d0089a65960c151b45ab3d06afba
Szymon <szymonphotoshop@gmail.com>
2020-02-11 15:24:31 +0100

    configured webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
 const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/script.js"
+  },
+  output: { path: path.resolve(__dirname, "build"), filename: "bundle.js" },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  }
+};
